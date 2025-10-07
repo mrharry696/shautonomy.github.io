67a2078f584491f86403c89273f21fb42df60072
mrharry696 <sambanasaibabu1233@gmail.com>
2025-10-07 19:03:21 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        border: "var(--border)",
+        primary: "var(--primary)",
+        "primary-foreground": "var(--primary-foreground)",
+      },
+    },
+  },
+  plugins: [],
+};
